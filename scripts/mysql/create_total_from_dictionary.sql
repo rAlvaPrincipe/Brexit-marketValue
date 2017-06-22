61d70cab832de080791eb2400d430aea4127e029
Marco <macatmail@gmail.com>
2017-06-22 13:15:10 +0200

    added .gitignore and removed DS_Store mac files

--- scripts/mysql/create_total_from_dictionary.sql
+++ scripts/mysql/create_total_from_dictionary.sql
@@ -1,15 +1,31 @@
-create table total
-select * from dictionary;
+-- dictionary is the union of
+
+alter table affin add column dictionary varchar(30);
+update affin set dictionary = "affin";
+
+alter table bing add column dictionary varchar(30);
+update bing set dictionary = "bing";
+
+create table dictionary
+(select * from affin)
+union
+(select * from bing);
+
+
+
+
+
+--
+
+create table total select * from dictionary;
 
 DELETE FROM total
 where word in (
-				select word from 
-								(select word, count(*) as num 
-								from dictionary 
-								group by word 
+				select word from
+								(select word, count(*) as num
+								from dictionary
+								group by word
 								having num = 2)
                                 as table_1
-				) 
+				)
 and dictionary = "affin"
-                
-
