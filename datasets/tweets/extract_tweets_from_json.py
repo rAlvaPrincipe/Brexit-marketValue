15792b7697f71d1b62e3d9011641e70894af7f91
Marco <macatmail@gmail.com>
2017-06-27 20:00:36 +0200

    extract json to sql from BIG file

--- datasets/tweets/extract_tweets_from_json.py
+++ datasets/tweets/extract_tweets_from_json.py
@@ -21,21 +21,48 @@ def extractDate(json_data):
     else:
         return 0
 
+# remove non ascii characters from string
+def _removeNonAscii(s): return "".join(i for i in s if ord(i)<128)
 
 
-
+# main
 def main():
-    tweets_file = "example.json"
+    tweet_sql_file = "tweets_resolved_brexit.sql"
+    # add the creation table at start
+    f = open(tweet_sql_file, 'a')
+    table_creation = """USE experiments;
+    CREATE TABLE IF NOT EXISTS tweets_resolved_brexit(
+        id INT UNSIGNED NOT NULL,
+        tweet varchar(255) NOT NULL,
+        tweet_date DATE NOT NULL,
+        PRIMARY KEY (id),
+        INDEX tweet_date (tweet_date)
+    );
 
+    """
+    f.write(table_creation)
+    f.close
+
+    # tweets json
+    tweets_file = "resolved_brexit.json"
+    counter = 0
+
+    f = open(tweet_sql_file, 'a')
+
+    # read line by line (rU heps because read one line at time)
     with open(tweets_file, 'rU') as lines:
         for line in lines:
             tweet_text = extractText(line)
             tweet_date = extractDate(line)
 
             if tweet_text != 0:
-                print(tweet_text)
-                print(tweet_date)
-                # and do things like insert in database
+                tweet_text = _removeNonAscii(tweet_text)
+                tweet_text = str(tweet_text).replace("'", "\\'")
+                counter += 1
+                query = "INSERT INTO tweets_resolved_brexit VALUES (\'"+str(counter)+"\',\'"+tweet_text+"\',\'"+str(tweet_date)+"\');\n"
+                f.write(query)
+
+    f.close
 
 
 if __name__ == "__main__":
