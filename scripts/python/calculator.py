9d511875624e7b6154e5cb532e7a8046e0ab951e
Valzo <valzelli.m@gmail.com>
2017-06-28 16:10:15 +0200

    generalized methods standard_sequence and variation sequence with 2x2 or 3x3 emission matrix. Changed input for calculate()

--- scripts/python/calculator.py
+++ scripts/python/calculator.py
@@ -10,59 +10,86 @@ class Calculator():
 
     # builds a sequence of observation with only positive/negative sentiment
     # it uses 0 for pos and 1 for negs 
-    # input: src_emission = "../file_path/.."
-    # output: [1, 0, 0, 0, 0, 1, 0, ...]
-    def boolean_standard_sequence(self, src_emission):
+    # input:
+    # src_emission = "../file_path/.."
+    # sentiment_discretization: 2 or 3 classes
+    # sentiment_tollerance: range for sentiment neutral (only for 3 classes)
+    # output 2 classes discretization: [1, 0, 0, 0, 0, 1, 0, ...]
+    # output 3 classes discretization: [1, 0, 2, 0, 0, 2, 0, ...]
+    def standard_sequence(self, src_emission, sentiment_discretization, sentiment_tollerance):
         sentiment = mt.extract(src_emission)
         sequence = []
-        for count in range(0, sentiment.__len__()):
-            if sentiment[count][1] > 0:
-                sequence.extend([0])
-            elif sentiment[count][1] <= 0:
-                sequence.extend([1]) 
+        if sentiment_discretization == 2:
+            for count in range(0, sentiment.__len__()):
+                if sentiment[count][1] > 0:
+                    sequence.extend([0])
+                elif sentiment[count][1] <= 0:
+                    sequence.extend([1])
 
+        if sentiment_discretization == 3:
+            for count in range(0, sentiment.__len__()):
+                if sentiment[count][1] > sentiment_tollerance:
+                    sequence.extend([0])
+                elif sentiment[count][1] < -sentiment_tollerance:
+                    sequence.extend([2])
+                else:
+                    sequence.extend([1])
         return sequence
 
     # build a sequence of observation based on sentiment variation
     # it uses 0 for sale and 1 for scende
-    def boolean_variation_sequence(self, src_emission, tollerance_var):
-        sentiment = mt.delta_labels(src_emission, tollerance_var, 1) 
+    # input:
+    # sentiment_discretization: 2 or 3 classes
+    # sentiment_tollerance: range for sentiment neutral (only for 3 classes)
+    # output 2 classes discretization: [1, 0, 0, 0, 0, 1, 0, ...]
+    # output 3 classes discretization: [1, 0, 2, 0, 0, 2, 0, ...]
+    def variation_sequence(self, src_emission, sentiment_discretization, sentiment_tollerance):
+        sentiment = mt.delta_labels(src_emission, sentiment_tollerance, 1)
         sequence = []
-        for count in range(0, sentiment.__len__()):
-            if float(sentiment[count]) > float(tollerance_var):
-                sequence.extend([0])
-            elif float(sentiment[count]) <= float(tollerance_var):
-                sequence.extend([1])
+        if sentiment_discretization == 2:
+            for count in range(0, sentiment.__len__()):
+                if float(sentiment[count]) > 0:
+                    sequence.extend([0])
+                elif float(sentiment[count]) <= 0:
+                    sequence.extend([1])
+        if sentiment_discretization == 3:
+            for count in range(0, sentiment.__len__()):
+                if float(sentiment[count]) > float(sentiment_tollerance):
+                    sequence.extend([0])
+                elif float(sentiment[count]) <= -float(sentiment_tollerance):
+                    sequence.extend([2])
+                else:
+                    sequence.extend([1])
         return sequence
 
 
     # build a sequence of observation based on normalized sentiment variation
-    def boolean_normalized_sequence(self, src_emission, tollerance_norm):
-        sentiment = mt.extract(src_emission)
-        sentiment = self.normalize(sentiment)
-        sequence = []
-        for count in range(0, sentiment.__len__()):
-            if float(sentiment[count]) > float(tollerance_norm):
-                sequence.extend([0])
-            elif float(sentiment[count]) <= float(tollerance_norm):
-                sequence.extend([1])
-        return sequence
+#    def boolean_normalized_sequence(self, src_emission, tollerance_norm):
+#        sentiment = mt.extract(src_emission)
+#        sentiment = self.normalize(sentiment)
+#        sequence = []
+#        for count in range(0, sentiment.__len__()):
+#            if float(sentiment[count]) > float(tollerance_norm):
+#                sequence.extend([0])
+#            elif float(sentiment[count]) <= float(tollerance_norm):
+#                sequence.extend([1])
+#        return sequence
+#
+#    def normalize(self, list):
+#        # compute alpha: the normalization variable
+#        np_list=np.array(list)
+#        float_list=[]
+#        for count in range(0, np_list.__len__()):
+#            float_list.append(float(np_list[count][1]))
+#
+#        max_v=max(float_list[:])
+#        min_v=min(float_list[:])
+#
+#        normalized_list = []
+#        for count in range(0, float_list.__len__()):
+#            normalized_list.append((float_list[count] - min_v)/(max_v - min_v))
 
-    def normalize(self, list):
-        # compute alpha: the normalization variable
-        np_list=np.array(list)
-        float_list=[]
-        for count in range(0, np_list.__len__()):
-            float_list.append(float(np_list[count][1]))
-
-        max_v=max(float_list[:])
-        min_v=min(float_list[:])
-
-        normalized_list = []
-        for count in range(0, float_list.__len__()):
-            normalized_list.append((float_list[count] - min_v)/(max_v - min_v))
-
-        return normalized_list
+#        return normalized_list
 
         # count the correspondence between the real state sequence adn the predicted sequence
 
@@ -102,7 +129,7 @@ class Calculator():
         print(prediction)
         return str(count_corr / float(state.__len__() - 1))
 
-    def start(self, vocabulary_request, sentiment_type, tollerance, tollerance_var, tollerance_norm):
+    def start(self, vocabulary_request, sentiment_type, market_tollerance, sentiment_tollerance, sentiment_discretization):
         days = ['2016/12/05', '2016/12/06', '2016/12/07', '2016/12/08', '2016/12/09',
                 '2016/12/12', '2016/12/13', '2016/12/14', '2016/12/15', '2016/12/16',
                 '2016/12/19', '2016/12/20', '2016/12/21', '2016/12/22', '2016/12/23',
@@ -117,55 +144,74 @@ class Calculator():
         #    print(days_sentiment[i])
         #out_file.close()
 
-        src = "../../datasets/Market_values.txt"
-        src_ext = "../../datasets/Market_values_ext.txt"
+        #src = "../../datasets/Market_values.txt"
+        #src_ext = "../../datasets/Market_values_ext.txt"
 
         # for valzo
-        #src = "D:\Dropbox\Git_Projects\Brexit-marketValue\datasets\Market_values.txt"
-        #src_ext = "D:\Dropbox\Git_Projects\Brexit-marketValue\datasets\Market_values_ext.txt"
+        src = "D:\Dropbox\Git_Projects\Brexit-marketValue\datasets\Market_values.txt"
+        src_ext = "D:\Dropbox\Git_Projects\Brexit-marketValue\datasets\Market_values_ext.txt"
 
         src_emission = "Sentiment.txt"
         predicted_sequence_filtering = []
         predicted_sequence_viterbi = []
 
-        T = mt.build_transition_m(mt.extract(src_ext), tollerance)
+        T = mt.build_transition_m(mt.extract(src_ext), market_tollerance)
         I = [1.0 / 3.0, 1.0 / 3.0, 1.0 / 3.0]
-        delta_stock = mt.delta(mt.extract(src), tollerance)
+        delta_stock = mt.delta(mt.extract(src), market_tollerance)
+
 
         if sentiment_type == "standard":
-            O = mt.build_emission_generic(delta_stock,
+
+            if sentiment_discretization == 2:
+                O = mt.build_emission_generic(delta_stock,
                                               [["sale", "sale"], ["stabile", "stabile"], ["scende", "scende"]],
-                                              self.boolean_standard_sequence(src_emission),
+                                              self.standard_sequence(src_emission, 2 , sentiment_tollerance),
                                               [["sent+", "0"], ["sent-", "1"]]
                                               )
+            if sentiment_discretization == 3:
+                O = mt.build_emission_generic(delta_stock,
+                                              [["sale", "sale"], ["stabile", "stabile"], ["scende", "scende"]],
+                                              self.standard_sequence(src_emission, 3 , sentiment_tollerance),
+                                              [["sent+", "0"], ["sent=", "1"], ["sent-", "2"]]
+                                              )
+
             model = Hmm(T, O, I)
             print("\n\nFiltering:")
-            predicted_sequence_filtering = model.filtering(19, self.boolean_standard_sequence(src_emission))
+            predicted_sequence_filtering = model.filtering(19, self.standard_sequence(src_emission, sentiment_discretization, sentiment_tollerance))
             print("\n\nViterbi:")
-            predicted_sequence_viterbi = model.viterbi(self.boolean_standard_sequence(src_emission))
+            predicted_sequence_viterbi = model.viterbi(self.standard_sequence(src_emission, sentiment_discretization, sentiment_tollerance))
         
         elif sentiment_type == "variation":
-            O = mt.build_emission_generic(delta_stock,
+
+            if sentiment_discretization == 2:
+                O = mt.build_emission_generic(delta_stock,
                                               [["sale", "sale"], ["stabile", "stabile"], ["scende", "scende"]],
-                                              mt.delta_labels(src_emission, tollerance_var, 2),
-                                              [["sentSale", "sale"], ["sentStabile", "scende"],
-                                               ["sentScende", "scende"]]
+                                              self.variation_sequence(src_emission, sentiment_discretization,
+                                                                      sentiment_tollerance),
+                                              [["sentSale", "0"], ["sentScende", "1"]]
+                                              )
+            if sentiment_discretization == 3:
+                O = mt.build_emission_generic(delta_stock,
+                                              [["sale", "sale"], ["stabile", "stabile"], ["scende", "scende"]],
+                                              self.variation_sequence(src_emission, sentiment_discretization,
+                                                                      sentiment_tollerance),
+                                              [["sentSale", "0"], ["sentStabile", "1"], ["sentScende", "2"]]
                                               )
             mt.delta(mt.extract(src_emission), 0.0)
             model = Hmm(T, O, I)
             print("\n\nFiltering:")
-            predicted_sequence_filtering = model.filtering(19, self.boolean_variation_sequence(src_emission, tollerance_var))
+            predicted_sequence_filtering = model.filtering(19, self.variation_sequence(src_emission, sentiment_discretization, sentiment_tollerance))
             print("\n\nViterbi:")
-            predicted_sequence_viterbi = model.viterbi(self.boolean_variation_sequence(src_emission, tollerance_var))
+            predicted_sequence_viterbi = model.viterbi(self.variation_sequence(src_emission, sentiment_discretization, sentiment_tollerance))
 
-        elif sentiment_type == "normalized":
-            O = mt.build_emission_m(delta_stock, self.boolean_normalized_sequence(src_emission, tollerance_norm))
-
-            model = Hmm(T, O, I)
-            print("\n\nFiltering:")
-            predicted_sequence_filtering = model.filtering(19, self.boolean_normalized_sequence(src_emission, tollerance_norm))
-            print("\n\nViterbi:")
-            predicted_sequence_viterbi = model.viterbi(self.boolean_normalized_sequence(src_emission, tollerance_norm))
+#        elif sentiment_type == "normalized":
+#            O = mt.build_emission_m(delta_stock, self.boolean_normalized_sequence(src_emission, tollerance_norm))
+#
+#            model = Hmm(T, O, I)
+#            print("\n\nFiltering:")
+#            predicted_sequence_filtering = model.filtering(19, self.boolean_normalized_sequence(src_emission, tollerance_norm))
+#            print("\n\nViterbi:")
+#            predicted_sequence_viterbi = model.viterbi(self.boolean_normalized_sequence(src_emission, tollerance_norm))
 
 
 
