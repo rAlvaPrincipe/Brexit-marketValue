ad66d9205223737e10934bbb4dfff467470b702a
Marco <macatmail@gmail.com>
2017-07-03 14:46:20 +0200

    first commit refactoring

---
+++ data/scripts/txt2sql_BING.py
@@ -0,0 +1,54 @@
+# retrive all the data
+positive_file = "BING_positive_words.txt"
+negative_file = "BING_negative_words.txt"
+positive_words =  []
+negative_words =  []
+positive_value = 2
+negative_value = -2
+
+#fill the positive words list
+for line in open(positive_file):
+    if line[0] != ";" and line[0] != "\n":
+        line = line.replace("\n", "")
+        positive_words.append(line)
+#fill the negative words list
+for line in open(negative_file):
+    if line[0] != ";" and line[0] != "\n":
+        line = line.replace("\n", "")
+        negative_words.append(line)
+
+#WARNING! Three words (envious,enviously,enviousness ) are positive AND negative.
+#we remove these words from positive_words list
+for i in positive_words:
+        if i in negative_words:
+            positive_words.remove(str(i))
+for i in negative_words:
+        if i in positive_words:
+            positive_words.remove(str(i))
+
+#todo ^ why double check? investigate.
+
+# add the creation table at start
+f = open('BING.sql', 'a')
+table_creation = """USE experiments;
+CREATE TABLE IF NOT EXISTS bing(
+    word varchar(255) NOT NULL,
+    label int NOT NULL,
+    PRIMARY KEY (word)
+);
+"""
+f.write(table_creation)
+f.close
+
+# append positive words
+for word in positive_words:
+    f = open('BING.sql', 'a')
+    f.write("INSERT INTO bing VALUES (\'"+str(word).replace("'", "\\'")+"\', \'"+str(positive_value)+"\');\n")
+
+# append negative words
+for word in negative_words:
+    f = open('BING.sql', 'a')
+    f.write("INSERT INTO bing VALUES (\'"+str(word).replace("'", "\\'")+"\', \'"+str(negative_value)+"\');\n")
+
+
+f.close()
--- scripts/donwload_brexit_dataset.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget  https://www.dropbox.com/s/x93vq030dhtvuro/Brexit.zip
-unzip Brexit.zip && rm Brexit.zip
-rm  -rf __MACOSX 
-mv brexit.sql ../datasets/
-mv GBP_EUR.txt ../datasets/
